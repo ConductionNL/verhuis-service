8f41dd021a1987c054dad0785752b3fc37eb4921
Ruben van der Linde <ruben@conduction.nl>
2020-06-26 10:22:01 +0200

    scripting for setting of bsn

--- api/src/Service/VerhuisService.php
+++ api/src/Service/VerhuisService.php
@@ -37,24 +37,17 @@ class VerhuisService
 
     public function setBsn(array $task, array $resource)
     {
-        // We want to force product shizle
-        $ceremonieOfferId = $this->commonGroundService->getUuidFromUrl($resource['properties']['plechtigheid']);
-        switch ($ceremonieOfferId) {
-            case "1ba1772b-cc8a-4808-ad1e-f9b3c93bdebf": // Flits huwelijks
-                $resource['properties']['ambtenaar'] = $this->commonGroundService->cleanUrl(['component' => 'pdc', 'type' => 'offers', 'id' => '55af09c8-361b-418a-af87-df8f8827984b']);
-                $resource['properties']['locatie'] = $this->commonGroundService->cleanUrl(['component' => 'pdc', 'type' => 'offers', 'id' => '9aef22c4-0c35-4615-ab0e-251585442b55']);
-                break;
-            case "77f6419d-b264-4898-8229-9916d9deccee": // Gratis trouwen
-                $resource['properties']['ambtenaar'] = $this->commonGroundService->cleanUrl(['component' => 'pdc', 'type' => 'offers', 'id' => '55af09c8-361b-418a-af87-df8f8827984b']);
-                $resource['properties']['locatie'] = $this->commonGroundService->cleanUrl(['component' => 'pdc', 'type' => 'offers', 'id' => '7a3489d5-2d2c-454b-91c9-caff4fed897f']);
-                break;
-            case "2b9ba0a9-376d-45e2-aa83-809ef07fa104": // Eenvoudig trouwen
-                $resource['properties']['ambtenaar'] = $this->commonGroundService->cleanUrl(['component' => 'pdc', 'type' => 'offers', 'id' => '55af09c8-361b-418a-af87-df8f8827984b']);
-                $resource['properties']['locatie'] = $this->commonGroundService->cleanUrl(['component' => 'pdc', 'type' => 'offers', 'id' => '7a3489d5-2d2c-454b-91c9-caff4fed897f']);
-                break;
-            case "bfeb9399-fce6-49b8-a047-70928f3611fb": // Uitgebreid trouwen
-                // In het geval van uitgebreid trouwen hoeven we niks te forceren
-                break;
+        $wiebsn = [];
+        if(array_key_exists('wie',$resource['properties'])){
+            $wie = str_replace(["[","}"],"",$resource['properties']['wie']);
+            $wie = explode(",", $wie);
+            foreach ($wie as $brpurl){
+                $wiebsn[] = $this->commonGroundService->getUuidFromUrl($brpurl);
+            }
         }
+
+        $resource['properties']['wiebsn'] = $wiebsn;
+
+        return $resource;
     }
 }
